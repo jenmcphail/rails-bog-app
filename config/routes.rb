16e30882cb9caacc4e876f42ad7a859a17396c30
jenmcphail <mcphail.jen@gmail.com>
2016-06-15 12:20:57 -0700

    adds new, create, and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   root "creatures#index"
 
+  #GETs index page
   get "/creatures", to: "creatures#index", as: "creatures"
+
+  #GETs form for new creature
+  get "/creatures/new", to: "creatures#new"
+
+  #POSTs a new creature using the form to the creatures index page
+  post "/creatures", to: "creatures#create"
+
+  #SHOWs individual creature
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 
 
 
